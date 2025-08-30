5d0bf9df10d31dc212c39ce33795ca6e6dae37c9
Sergey Rutsky <rutsky.s.v@gmail.com>
2025-08-30 17:09:08 +0300

    Add_CasinoGameBase

---
+++ FinalTask/Games/CasinoGameBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask.Games
+{
+    public abstract class CasinoGameBase
+    {
+        public abstract void PlayGame();
+        public event Action? OnWin;
+        public event Action? OnLoose;
+        public event Action? OnDraw;
+        protected void OnWinInvoke()
+        {
+            OnWin?.Invoke();
+        }
+        protected void OnLooseInvoke()
+        {
+            OnLoose?.Invoke();
+        }
+        protected void OnDrawInvoke()
+        {
+            OnDraw?.Invoke();
+        }
+        public CasinoGameBase()
+        {
+            FactoryMethod();
+        }
+        protected abstract void FactoryMethod();
+        public void Result(string result)
+        {
+            Console.WriteLine(result);
+        }
+
+
+
+    }
+}
